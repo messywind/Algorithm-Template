f68700873265dab623ea04df9cbcc4254ecf059a
messywind <54348627+messywind@users.noreply.github.com>
2022-05-19 12:49:59 +0800

    Create Sparse Table.cpp

---
+++ Data Structure/Sparse Table.cpp
@@ -0,0 +1,28 @@
+struct ST {
+    vector<vector<int>> f, g, h;
+    ST(int n, int m, vector<int> a) : f(n), g(n), h(n) {
+        for (int i = 1; i < n; i ++) {
+            f[i].resize(m), g[i].resize(m), h[i].resize(m);
+            f[i][0] = g[i][0] = h[i][0] =  a[i];
+        }
+        for (int j = 1; (1ll << j) < n; j ++) {
+            for (int i = 1; i + (1ll << j) - 1 < n; i ++) {
+                f[i][j] = max(f[i][j - 1], f[i + (1ll << (j - 1))][j - 1]);
+                g[i][j] = min(g[i][j - 1], g[i + (1ll << (j - 1))][j - 1]);
+                h[i][j] = __gcd(h[i][j - 1], h[i + (1ll << (j - 1))][j - 1]);
+            }
+        }
+    }
+    int askMax(int l, int r) {
+        int k = __lg(r - l + 1);
+        return max(f[l][k], f[r - (1ll << k) + 1][k]);
+    }
+    int askMin(int l, int r) {
+        int k = __lg(r - l + 1);
+        return min(g[l][k], g[r - (1ll << k) + 1][k]);
+    }
+    int askGcd(int l, int r) {
+        int k = __lg(r - l + 1);
+        return __gcd(h[l][k], h[r - (1ll << k) + 1][k]);
+    }
+};
