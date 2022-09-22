3406f0aa6991a3d72b8326d159d407a3a87d1887
messywind <54348627+messywind@users.noreply.github.com>
2022-09-22 14:39:09 +0800

    Update Fenwick.cpp

--- Data Structure/Fenwick.cpp
+++ Data Structure/Fenwick.cpp
@@ -1,13 +1,18 @@
+template <typename T>
 struct Fenwick {
     int n;
-    vector<int> tr;
-    Fenwick(int n) : n(n), tr(n) {}
-    void modify(int x, int c) {
-        for (int i = x + 1; i <= n; i += i & -i) tr[i - 1] += c;
+    vector<T> a;
+    Fenwick(int n) : n(n), a(n) {}
+    void modify(int x, T v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] += v;
+        }
     }
-    int ask(int x) {
-        int res = 0;
-        for (int i = min(x + 1, n); i; i -= i & -i) res += tr[i - 1];
-        return res;
+    T ask(int x) {
+        T ans = 0;
+        for (int i = min(x + 1, n); i; i -= i & -i) {
+            ans += a[i - 1];
+        }
+        return ans;
     }
 };
